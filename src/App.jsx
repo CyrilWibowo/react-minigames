86c54e5a7411ca921be5514947b8617590dc76d6
Cyril Wibowo <cd.wibowo@gmail.com>
2025-12-05 17:20:08 +1100

    Update App to integrate Blanko game in routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
+import Blanko from './components/Blanko';
 import './App.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <main className="main-body">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/blanko" element={<div>Blanko Game</div>} />
+          <Route path="/blanko" element={<Blanko />} />
           <Route path="/slido" element={<div>Slido Game</div>} />
           <Route path="/tetro" element={<div>Tetro Game</div>} />
         </Routes>
